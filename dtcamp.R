6e62aa2c9e311ce37f8f9652d30beabd157040f1
Arthur Thina <arthur.thina@gmail.com>
2022-02-17 16:04:36 -0300

    initial commit

---
+++ dtcamp.R
@@ -0,0 +1,34 @@
+library(RSQLite)
+
+data("mtcars")
+mtcars$car_names <- rownames(mtcars)
+rownames(mtcars) <- c()
+head(mtcars)
+
+conn <- dbConnect(RSQLite::SQLite(), "CarsDB.db")
+
+
+dbWriteTable(conn, "cars_data", mtcars)
+
+dbListTables(conn)
+
+
+car <- c('Camaro', 'California', 'Mustang', 'Explorer')
+make <- c('Chevrolet','Ferrari','Ford','Ford')
+df1 <- data.frame(car,make)
+car <- c('Corolla', 'Lancer', 'Sportage', 'XE')
+make <- c('Toyota','Mitsubishi','Kia','Jaguar')
+df2 <- data.frame(car,make)
+
+dfList <- list(df1,df2)
+
+# Write a table by appending the data frames inside the list
+for(k in 1:length(dfList)){
+  dbWriteTable(conn,"Cars_and_Makes", dfList[[k]], append = TRUE)
+}
+
+dbListTables(conn)
+
+dbGetQuery(conn, "SELECT * FROM Cars_and_Makes")
+
+dbGetQuery(conn, "SELECT * FROM cars_data LIMIT 10")
